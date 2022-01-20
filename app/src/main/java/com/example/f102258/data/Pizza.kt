ffa3f8bafa0dd1271b4e63c37979956e491e24f0
Aleksandar Geyman <ageyman@vmware.com>
2022-01-20 17:56:50 +0200

    Added pizzas.json and logic to create pizza objects.

---
+++ app/src/main/java/com/example/f102258/data/Pizza.kt
@@ -0,0 +1,9 @@
+package com.example.f102258.data
+
+data class Pizza(
+    val id: Int,
+    val name: String,
+    val description: String,
+    val size: String,
+    val price: Double
+)
---
+++ app/src/main/java/com/example/f102258/data/PizzasRepository.kt
@@ -0,0 +1,29 @@
+package com.example.f102258.data
+
+import android.content.Context
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import java.io.IOException
+
+class PizzasRepository(private val context: Context) {
+
+    // Returns List of Pizza objects.
+    fun getPizzas(): List<Pizza> {
+        val jsonFileString = getJsonDataFromAsset(context, "pizzas.json")
+        val gson = Gson()
+        val listPizzaType = object : TypeToken<List<Pizza>>() {}.type
+       return gson.fromJson(jsonFileString, listPizzaType)
+    }
+
+    // Returns optional string from a json file in app assets if the file exists.
+   private fun getJsonDataFromAsset(context: Context, fileName: String): String? {
+        val jsonString: String
+        try {
+            jsonString = context.assets.open(fileName).bufferedReader().use { it.readText() }
+        } catch (ioException: IOException) {
+            ioException.printStackTrace()
+            return null
+        }
+        return jsonString
+    }
+}
