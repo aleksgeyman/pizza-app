11555ebf03a3e217155883910c21ff6a21d52c70
Aleksandar Geyman <ageyman@vmware.com>
2022-01-21 19:51:05 +0200

    Added option to send email.

--- app/src/main/java/com/example/f102258/fragments/OrderInformationFragment.kt
+++ app/src/main/java/com/example/f102258/fragments/OrderInformationFragment.kt
@@ -1,20 +1,21 @@
 package com.example.f102258.fragments
 
 import android.app.DatePickerDialog
+import android.content.Intent
 import android.icu.text.SimpleDateFormat
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
-import android.widget.Spinner
-import android.widget.TextView
+import android.widget.*
 import com.example.f102258.R
 import java.util.*
 
 class OrderInformationFragment : Fragment() {
 var selectDateTV: TextView? = null
+    var submitOrderButton: Button? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -31,7 +32,8 @@ var selectDateTV: TextView? = null
             )
         }
         spinner.adapter = adapter
-        selectDateTV = rootView.findViewById<TextView>(R.id.select_date_tv)
+        selectDateTV = rootView.findViewById(R.id.select_date_tv)
+        submitOrderButton = rootView.findViewById(R.id.submit_button)
         selectDateTV?.setOnClickListener { clickDatePicker() }
         return rootView
     }
@@ -46,6 +48,7 @@ var selectDateTV: TextView? = null
             { _, selYear, selMonth, dayOfMonth ->
                 val selectedDate = "$dayOfMonth/${selMonth + 1}/$selYear"
                 selectDateTV?.text = selectedDate
+                submitOrderButton?.isEnabled = true
             },
             year,
             month,
@@ -53,5 +56,34 @@ var selectDateTV: TextView? = null
         )
         dpd.datePicker.minDate = System.currentTimeMillis()
         dpd.show()
+    }
+
+    private fun sendEmail(recipient: String, subject: String, message: String) {
+        /*ACTION_SEND action to launch an email client installed on your Android device.*/
+        val mIntent = Intent(Intent.ACTION_SEND)
+        /*To send an email you need to specify mailto: as URI using setData() method
+        and data type will be to text/plain using setType() method*/
+        mIntent.data = Uri.parse("mailto:")
+        mIntent.type = "text/plain"
+        // put recipient email in intent
+        /* recipient is put as array because you may wanna send email to multiple emails
+           so enter comma(,) separated emails, it will be stored in array*/
+        mIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf(recipient))
+        //put the Subject in the intent
+        mIntent.putExtra(Intent.EXTRA_SUBJECT, subject)
+        //put the message in the intent
+        mIntent.putExtra(Intent.EXTRA_TEXT, message)
+
+
+        try {
+            //start email intent
+            startActivity(Intent.createChooser(mIntent, "Choose Email Client..."))
+        }
+        catch (e: Exception){
+            //if any thing goes wrong for example no email client application or any exception
+            //get and show exception message
+            Toast.makeText(activity, e.message, Toast.LENGTH_LONG).show()
+        }
+
     }
 }
