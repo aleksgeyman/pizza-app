d078fbb79c7fd1ea464e7b79b0081fdf95526343
Aleksandar Geyman <ageyman@vmware.com>
2022-01-21 17:16:10 +0200

    Added spinner to select current city location to deliver.

--- app/src/main/java/com/example/f102258/fragments/OrderInformationFragment.kt
+++ app/src/main/java/com/example/f102258/fragments/OrderInformationFragment.kt
@@ -5,18 +5,25 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import com.example.f102258.R
 
 class OrderInformationFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_order_information, container, false)
+        val rootView = inflater.inflate(R.layout.fragment_order_information, container, false)
+
+        val cities = resources.getStringArray(R.array.cities_array)
+        val spinner = rootView.findViewById<Spinner>(R.id.cities_spinner)
+
+          val adapter =
+              activity?.let { ArrayAdapter(it, R.layout.support_simple_spinner_dropdown_item, cities) }
+            spinner.adapter = adapter
+            return rootView
+        }
     }
-}
