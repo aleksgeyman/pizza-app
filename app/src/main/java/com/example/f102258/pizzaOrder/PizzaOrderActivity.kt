34f12de25fa501e7200df909a78d8c5e7d7f55e8
Aleksandar Geyman <ageyman@vmware.com>
2022-01-21 16:35:29 +0200

    Added logic to set and update number of pizzas to order.

--- app/src/main/java/com/example/f102258/pizzaOrder/PizzaOrderActivity.kt
+++ app/src/main/java/com/example/f102258/pizzaOrder/PizzaOrderActivity.kt
@@ -2,17 +2,24 @@ package com.example.f102258.pizzaOrder
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import android.widget.TextView
 import androidx.activity.viewModels
 import com.example.f102258.R
+import com.example.f102258.data.Pizza
 import com.example.f102258.pizzasList.PizzasListViewModel
 import com.example.f102258.pizzasList.PizzasListViewModelFactory
 
 class PizzaOrderActivity : AppCompatActivity() {
-
     private val viewModel by viewModels<PizzaOrderViewModel> {
         PizzaOrderViewModelFactory(this)
     }
+
+    private var pizzaTotalPriceText: TextView? = null
+    private var pizzaCountText: TextView? = null
+    private var addPizzaButton: Button? = null
+    private var removePizzaButton: Button? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,9 +30,35 @@ class PizzaOrderActivity : AppCompatActivity() {
         val currentPizzaId = intent.extras?.getInt(R.string.pizza_id.toString())
         currentPizzaId?.let {
            val currentPizza = viewModel.getPizza(it)
+            viewModel.updateTotalPrice()
             pizzaNameTextView.text = currentPizza?.name
             pizzaDescriptionTextView.text = currentPizza?.description
             pizzaPriceTextView.text = currentPizza?.price.toString()
         }
+        configureButtons()
+        configureTextViews()
+    }
+
+    private fun configureTextViews() {
+        pizzaCountText = findViewById(R.id.pizzas_count_tv)
+        viewModel.orderCount.observe(this, {
+            it?.let {
+                pizzaCountText?.text = it.toString()
+            }
+        })
+
+        pizzaTotalPriceText = findViewById(R.id.pizza_price)
+        viewModel.totalPrice.observe(this, {
+            it?.let {
+                pizzaTotalPriceText?.text = it.toString()
+            }
+        })
+    }
+
+    private fun configureButtons() {
+        addPizzaButton = findViewById(R.id.increment_button)
+        addPizzaButton?.setOnClickListener { viewModel.addPizza() }
+        removePizzaButton = findViewById(R.id.decrement_button)
+        removePizzaButton?.setOnClickListener { viewModel.removePizza() }
     }
 }
--- app/src/main/java/com/example/f102258/pizzaOrder/PizzaOrderViewModel.kt
+++ app/src/main/java/com/example/f102258/pizzaOrder/PizzaOrderViewModel.kt
@@ -1,15 +1,42 @@
 package com.example.f102258.pizzaOrder
 
 import android.content.Context
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.example.f102258.data.Pizza
 import com.example.f102258.data.PizzasRepository
 
 class PizzaOrderViewModel(private val pizzasRepository: PizzasRepository) : ViewModel() {
+    var pizza: Pizza? = null
+    var orderCount = MutableLiveData<Int>(1)
+    var totalPrice = MutableLiveData<Double>(0.0)
+
+    fun addPizza() {
+        if (orderCount.value!! < 10) {
+            orderCount.value = orderCount!!.value!!.plus(1)
+            updateTotalPrice()
+        }
+    }
+
+    fun removePizza() {
+        if (orderCount.value!! > 1) {
+            orderCount.value = orderCount!!.value!!.minus(1)
+            updateTotalPrice()
+        }
+    }
 
     fun getPizza(id: Int): Pizza? {
-        return pizzasRepository.getPizza(id)
+        pizza?.let { it ->
+            return it
+        }
+        val fetchedPizza = pizzasRepository.getPizza(id)
+        pizza = fetchedPizza
+        return fetchedPizza
+    }
+
+      fun updateTotalPrice() {
+         totalPrice.value = (pizza!!.price * orderCount.value!!)
     }
 }
 
