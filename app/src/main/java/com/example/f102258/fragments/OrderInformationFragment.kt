1237fedd57db8cd2e58b761cb46f33eaea277974
Aleksandar Geyman <ageyman@vmware.com>
2022-01-21 19:21:37 +0200

    Added city and date selection for order

--- app/src/main/java/com/example/f102258/fragments/OrderInformationFragment.kt
+++ app/src/main/java/com/example/f102258/fragments/OrderInformationFragment.kt
@@ -1,5 +1,7 @@
 package com.example.f102258.fragments
 
+import android.app.DatePickerDialog
+import android.icu.text.SimpleDateFormat
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -7,23 +9,49 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.Spinner
+import android.widget.TextView
 import com.example.f102258.R
+import java.util.*
 
 class OrderInformationFragment : Fragment() {
-
+var selectDateTV: TextView? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val rootView = inflater.inflate(R.layout.fragment_order_information, container, false)
-
         val cities = resources.getStringArray(R.array.cities_array)
         val spinner = rootView.findViewById<Spinner>(R.id.cities_spinner)
-
-          val adapter =
-              activity?.let { ArrayAdapter(it, R.layout.support_simple_spinner_dropdown_item, cities) }
-            spinner.adapter = adapter
-            return rootView
+        val adapter = activity?.let {
+            ArrayAdapter(
+                it,
+                R.layout.support_simple_spinner_dropdown_item,
+                cities
+            )
         }
+        spinner.adapter = adapter
+        selectDateTV = rootView.findViewById<TextView>(R.id.select_date_tv)
+        selectDateTV?.setOnClickListener { clickDatePicker() }
+        return rootView
     }
+
+    private fun clickDatePicker() {
+        val myCalendar = Calendar.getInstance()
+        val year = myCalendar.get(Calendar.YEAR)
+        val month = myCalendar.get(Calendar.MONTH)
+        val day = myCalendar.get(Calendar.DAY_OF_MONTH)
+        val dpd = DatePickerDialog(
+            requireActivity(),
+            { _, selYear, selMonth, dayOfMonth ->
+                val selectedDate = "$dayOfMonth/${selMonth + 1}/$selYear"
+                selectDateTV?.text = selectedDate
+            },
+            year,
+            month,
+            day
+        )
+        dpd.datePicker.minDate = System.currentTimeMillis()
+        dpd.show()
+    }
+}
